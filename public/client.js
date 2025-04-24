d783c33b58d19c8d93308bfc928f9463883f86d3
boujrade <hassanchikh455@gmail.com>
2025-04-24 09:59:48 +0200

    Delete public/client.js

--- public/client.js
+++
@@ -1,7 +0,0 @@
-fetch("/products")
-    .then(response => response.json())
-    .then(data => {
-        let content = data.map(product => `<h2>${product.name}</h2><p>${product.description}</p>`).join("");
-        document.getElementById("page-content").innerHTML = content;
-    })
-    .catch(error => console.error("خطأ في تحميل البيانات:", error));
